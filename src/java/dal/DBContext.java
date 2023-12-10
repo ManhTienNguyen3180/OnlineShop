a49b8b5474259caaf4d98b85aa4add23233a5dbb
tiennmhe <taixexedo@gmail.com>
2023-12-10 15:30:33 +0700

    fix connect string

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -8,29 +8,15 @@ import java.sql.SQLException;
 
 public class DBContext {
     protected Connection connection;
-    public DBContext() {
+    public DBContext()
+    {
         try {
-            // Edit the serverName, databaseName, user, and password to match your Azure SQL Database configuration
-            String serverName = "bookstore123.database.windows.net";
-            String databaseName = "Fasshop";
-            String user = "tiennm@bookstore123";
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://bookstore123.database.windows.net:1433;database=Fasshop";
+            String username = "tiennm@bookstore123";
             String password = "Tienlove012";
-
-            String url = String.format("jdbc:sqlserver://%s:1433;databaseName=%s;", serverName, databaseName);
-
-            // For Azure SQL Database, you should use the Azure-specific JDBC driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-            // Add additional properties for Azure SQL Database connection
-            java.util.Properties properties = new java.util.Properties();
-            properties.setProperty("user", user);
-            properties.setProperty("password", password);
-            properties.setProperty("encrypt", "true");
-            properties.setProperty("trustServerCertificate", "false");
-            properties.setProperty("hostNameInCertificate", "*.database.windows.net");
-            properties.setProperty("loginTimeout", "30");
-
-            connection = DriverManager.getConnection(url, properties);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println(ex);
         }
