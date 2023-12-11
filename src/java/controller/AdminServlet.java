de7063cec3f0f1e54c1dea57e75db423887a070b
Nguyễn Tiến <151045416+ManhTienNguyen3180@users.noreply.github.com>
2023-12-11 09:13:14 +0700

    Update AdminServlet.java

--- src/java/controller/AdminServlet.java
+++ src/java/controller/AdminServlet.java
@@ -62,7 +62,7 @@ public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.addHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         productDAO d = new productDAO();
@@ -81,7 +81,7 @@ public class AdminServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-       response.setHeader("Access-Control-Allow-Origin", "*");
+       response.addHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         productDAO o = new productDAO();
